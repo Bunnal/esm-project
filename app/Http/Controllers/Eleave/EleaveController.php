f45e88c11d559099c852011359cc14df9a8d1f56
@Bunal <leangbunal432@gmail.com>
2021-11-12 22:33:16 +0700

    update

--- app/Http/Controllers/Eleave/EleaveController.php
+++ app/Http/Controllers/Eleave/EleaveController.php
@@ -79,12 +79,14 @@ class EleaveController extends Controller
                 return $this->getDataToDisplay($leave_takes);
                 break;
             case 'week':
-                $leave_takes = LeaveTake::whereDate('startdate','>',  $formatted_date)->orderByDesc('id')->get(); 
-                return $this->getDataToDisplay($leave_takes);
+                $leave_takes=LeaveTake::whereDate('startdate','>',  $formatted_date)->orderByDesc('id')->get();
+                dd($leave_takes->count());
+                // return $this->getDataToDisplay($leave_takes);
                 break;
             case 'month':
                 $leave_takes = LeaveTake::whereMonth('startdate',date('m'))->orderByDesc('id')->get(); 
-                return $this->getDataToDisplay($leave_takes);
+                dd($leave_takes->count());
+                // return $this->getDataToDisplay($leave_takes);
                 break;
             default:
                 return abort(404);
